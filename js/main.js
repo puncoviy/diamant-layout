9f60f31591a5562b5fb252e2da63251566801275
puncoviy <usaspinner@gmail.com>
2022-08-12 12:56:31 +0300

    feat: add header section menu, fix scroll

--- js/main.js
+++ js/main.js
@@ -1,32 +1,62 @@
 let scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 300
+    speed: 500,
+    speedAsDuration: true,
+    offset: 200
 });
-
+// menu
+const burgerBtn = document.querySelector('.burger-box');
+const burgerIcon = document.querySelector('.burger-menu');
+const menuList = document.querySelector('.header__menu-list');
+const menuLinks = document.querySelectorAll('.header__menu-link');
+const blackout = document.querySelector('.blackout');
+// cart
 const cart = document.querySelectorAll('.catalogue__item-cart');
 const cartItem = document.querySelector('.header__cart-item');
-
+// sales_slide
 const prevSlide = document.querySelector('.sale__controls-left');
 const nextSlide = document.querySelector('.sale__controls-right');
 const slide = document.querySelectorAll('.sale__slider-item-inner');
 let currentSlide = 0;
-
+// authors_slide
 const authorImg = document.querySelector('.authors__image-img');
 const authorMin = document.querySelectorAll('.authors__min-img');
 
 
-cart.forEach(function(item, i){
-    item.addEventListener('click', function(){
-        cartItem.textContent++;
+// menu-Script
+burgerBtn.addEventListener('click', function(){
+    burgerIcon.classList.toggle('active');
+    menuList.classList.toggle('active');
+    blackout.classList.toggle('active');
+    menuLinks.forEach (el => el.addEventListener('click', function(){
+        burgerIcon.classList.remove('active');
+        menuList.classList.remove('active');
+        blackout.classList.remove('active');
+    }))
+    blackout.addEventListener('click', function(){
+        burgerIcon.classList.remove('active');
+        menuList.classList.remove('active');
+        blackout.classList.remove('active');
+    })
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 100) {
+            burgerIcon.classList.remove('active');
+            menuList.classList.remove('active');
+            blackout.classList.remove('active');
+        }
     })
 })
-
-
+// cart-Script
+cart.forEach(el => el.addEventListener('click', function(){
+        cartItem.textContent++;
+    })
+)
+// author_slide-Script
 authorMin.forEach(function(item, i) {
     item.addEventListener('click', function() {
         authorImg.src = authorMin[i].src;
     })
 })
-
+// sales_slide-Script
 prevSlide.addEventListener('click', function(){
     if (currentSlide > 0) {
         for (let i = 0; i < slide.length; i++) {
@@ -48,5 +78,3 @@ nextSlide.addEventListener('click', function(){
         return currentSlide;
     }
 })
-
-
